edc4cca4108c610d6a784de13699ba973fee951c
Xiaolu Lv <lvxiaolu_1997@163.com>
2022-07-07 22:42:23 +0800

    git init

---
+++ src/main/java/com/demo/lxl/LxlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.lxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxlDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/lxl/LxlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.lxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
